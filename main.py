2bf28867650f64372d306126074e6e3231316b4b
yjun0915 <kyjun09150916@gmail.com>
2025-03-06 13:09:37 +0900

    [func] compare result of Newton's method and BFGS

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from methods.NewtonsMethod import newton
+from scipy.optimize import minimize
 
-if __name__ == "__main__":
+def compare_newton_bfgs():
     p = lambda x: x**3 - x**2  - 1
     dp = lambda x: 3*x**2 - 2*x
+    bfgs = minimize(fun=p, x0=3, method='BFGS')
     approx = newton(f=p, df=dp, x0=3, epsilon=1, max_iter=10)
-    print(approx)
+    print(f"{bfgs.x} and {approx}")
+
+if __name__ == "__main__":
+    compare_newton_bfgs()
